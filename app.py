f5303bfa65beed4c58102148689c8f74f1d0b621
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2026-05-12 00:36:54 +0530

    feat: Flash messages + form validation added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request,flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -15,7 +15,7 @@ class Task(db.Model):
     assigned_to = db.Column(db.String(100), nullable=False)
     status = db.Column(db.String(20), default='Pending')
 
-    def __repr__(self):               # ← INSIDE the class. Indented.
+    def __repr__(self):              
         return f'<Task {self.title}>'
 
 
@@ -26,16 +26,24 @@ def home():
 
 @app.route('/tasks')
 def tasks():
-    all_tasks = Task.query.all()      # ← fetch from DB
+    all_tasks = Task.query.all()     
     return render_template('tasks.html', tasks=all_tasks)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_task():
     if request.method == 'POST':
-        title = request.form.get('title')
-        assigned_to = request.form.get('assigned_to')
+        title = request.form.get('title').strip()
+        assigned_to = request.form.get('assigned_to').strip()
         status = request.form.get('status')
+
+        if not title or not assigned_to:
+            flash('All fields are required!', 'danger')
+            return redirect(url_for('add_task'))
+        
+        if len(title)<3:
+            flash('Task title must be at least 3 characters!','warning')
+            return redirect(url_for('add_task'))
 
         new_task = Task(title=title, assigned_to=assigned_to, status=status)
         db.session.add(new_task)
@@ -50,11 +58,21 @@ def edit_task(task_id):
     task=Task.query.get_or_404(task_id)
 
     if request.method=='POST':
-        task.title=request.form.get('title')
-        task.assigned_to=request.form.get('assigned_to')
-        task.status=request.form.get('status')
+        title=request.form.get('title').strip()
+        assigned_to=request.form.get('assigned_to').strip()
+        status=request.form.get('status')
+
+        if not title or not assigned_to:
+            flash('All fields are required!','danger')
+            return redirect(url_for('edit_task',task_id=task_id))
+        
+        task.title=title
+        task.assigned_to=assigned_to
+        task.status=status
+
         db.session.add(task)
         db.session.commit()
+        flash('Task updated successfully!','success')
         return redirect(url_for('tasks'))
     
     return render_template('edit_task.html',task=task)
@@ -64,6 +82,7 @@ def delete_task(task_id):
     task=Task.query.get_or_404(task_id)
     db.session.delete(task)
     db.session.commit()
+    flash('Task updated successfully!','success')
     return redirect(url_for('tasks'))
 
 
